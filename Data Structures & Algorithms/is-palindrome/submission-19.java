6c36e7c8e4e8fef65230fea9fecb4e1636dac04f
Daniel <jamoja64749@gmail.com>
2026-08-14 13:45:33 +0200

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String word = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int R = word.length()-1;
+
+        for(int L = 0; L < word.length() - 1; L++){
+            if(word.charAt(L) != word.charAt(R)){
+                return false;
+            }
+            R--;
+        }
+
+        return true;
+
+    }
+}
