95e6653ed65217de91e5282b4e15517c897a6641
Daniel <jamoja64749@gmail.com>
2026-08-30 23:23:03 +0200

    Add: quickSort - submission-2

---
+++ Data Structures & Algorithms/quickSort/submission-2.java
@@ -0,0 +1,36 @@
+// Definition for a pair.
+// class Pair {
+//     int key;
+//     String value;
+//
+//     public Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    public List<Pair> quickSort(List<Pair> pairs) {
+        if (pairs.size() <= 1) return pairs;
+        
+        int n = pairs.size();
+        Pair pivot = pairs.get(n - 1);
+        int left = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (pairs.get(i).key < pivot.key) {
+                Pair temp = pairs.get(left);
+                pairs.set(left, pairs.get(i));
+                pairs.set(i, temp);
+                left++;
+            }
+        }
+
+        pairs.set(n - 1, pairs.get(left));
+        pairs.set(left, pivot);
+
+        quickSort(pairs.subList(0, left));
+        quickSort(pairs.subList(left + 1, n));
+
+        return pairs;
+    }
+}
