4fa8b46b285052b839f51f54c50a46f890cd4abb
Daniel <jamoja64749@gmail.com>
2026-09-19 21:54:03 +0200

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-4

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-4.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val;
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // inorder - left nodes -> root -> right nodes
+        // preorder - root  -> left node -> right node (root, left node, right node)
+        
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return helper(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = map.get(rootVal);
+
+        root.left = helper(preorder, inStart, mid - 1);
+        root.right = helper(preorder, mid + 1, inEnd);
+
+        return root;
+    }
+}
